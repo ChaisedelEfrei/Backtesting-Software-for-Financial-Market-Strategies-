ec133222ed26b9ea70224abeaa75f21cf737b9e9
Elodie COULLOC'H <elodie@Ordinateur-portable-de-Elodie.local>
2024-10-02 15:03:11 +0200

    creation of the template of the app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Backtesting_Software_for_Financial_Market_Strategies_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
